bfd26680a019aafa7eb4c34a161487e73cf6045d
mocanuandreii <andreialex47@gmail.com>
2018-03-27 09:19:13 +0300

    Add files via upload

--- Object.java
+++ Object.java
@@ -6,27 +6,39 @@ public class Object extends Smart Museums {
 
   public Integer height;
 
-  public Vector type;
+  public String type;
 
   public String Image;
 
     public Vector  myInformations;
     public Vector  myInformations;
     public Vector  myInformations;
-    public Vector  myGames;
+
     public Vector  mySmart Museums;
     public Vector  mySmart Museums;
 
   public void getHeight() {
+
+    this -> height = photoHeight;
+    return height;
   }
 
   public void getWidth() {
+
+    this -> width = photoHeight;
+    return height;
   }
 
   public void getImage() {
+  //gets Image
   }
 
   public void getType() {
+
+    this -> type = type[iterator];
+    return height;
+
   }
+
 
 }
